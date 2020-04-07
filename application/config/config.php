5fc199e1517981915215fd9f86c7dc7cc8e2a168
piyo02 <alzidni2000@gmail.com>
2020-04-07 14:18:36 +0800

    config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/nurul_iman/';
-$config['base_url'] = 'http://nuruliman.akar-z.com/';
+$config['base_url'] = 'https://nuruliman.akar-z.com/';
 
 /*
 |--------------------------------------------------------------------------
