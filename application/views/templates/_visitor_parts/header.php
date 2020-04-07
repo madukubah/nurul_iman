a2570bf69f9447892f776e6a7963f415ed2b1cdf
piyo02 <alzidni2000@gmail.com>
2020-04-07 14:43:08 +0800

    a

--- application/views/templates/_visitor_parts/header.php
+++ application/views/templates/_visitor_parts/header.php
@@ -2,5 +2,5 @@
     <div class="container-fluid">
       <div id="logo" class="pull-left">
         <!-- <h1><a href="#intro" class="scrollto">Nurul Iman</a></h1> -->
-        <a href="<?= base_url('') ?>"><img src="<?= base_url('front-assets/') ?>img/favicon.png" alt="" title="" /></a>
+        <a href="<?= base_url('') ?>"><img src="<?= base_url('assets/') ?>logo/coreigniter.png" alt="" title="" /></a>
       </div>
