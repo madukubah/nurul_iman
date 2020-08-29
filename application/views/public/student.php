3c7e7c7ce6c619971e1e714af7c8e24970c019e9
madukubah <muhalfalah1998@gmail.com>
2020-08-29 19:43:23 +0800

    tes

--- application/views/public/student.php
+++ application/views/public/student.php
@@ -7,38 +7,46 @@
       <div class="container">
 
         <header class="section-header">
-          <h3 class="section-title">Our Portfolio</h3>
+          <h3 class="section-title">Santri</h3>
         </header>
 
         <div class="row">
           <div class="col-lg-12">
             <ul id="portfolio-flters">
               <li data-filter="*" class="filter-active">Santri</li>
-              <li data-filter=".filter-app"><?= $student->registration_number ?></li>
+                <?php if( isset($student) ) : ?>
+                  <li data-filter=".filter-app"><?= $student->registration_number ?></li>
+                <?php endif; ?>
             </ul>
           </div>
         </div>
 
         <div class="row portfolio-container">
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp"></div>
-          
-          <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp">
-            <div class="portfolio-wrap">
-              <figure>
-                <img src="<?= $student->image ?>" class="img-fluid" alt="">
-                <a href="<?= $student->image ?>" data-lightbox="portfolio" data-title="<?= $student->name ?>" class="link-preview" title="Preview"><i class="ion ion-eye"></i></a>
-                <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
-              </figure>
+          <?php if( isset($student) ) : ?>
+              
+            <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp"></div>
+            
+            <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp">
+              <div class="portfolio-wrap">
+                <figure>
+                  <img src="<?= $student->image ?>" class="img-fluid" alt="">
+                  <a href="<?= $student->image ?>" data-lightbox="portfolio" data-title="<?= $student->name ?>" class="link-preview" title="Preview"><i class="ion ion-eye"></i></a>
+                  <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
+                </figure>
 
-              <div class="portfolio-info">
-                <h4><a href="#"><?= $student->name ?></a></h4>
-                <p><?= $student->registration_number ?></p>
-                <p><?= $student->ttl ?></p>
-                <p><?= $student->address ?></p>
-                <p><?= $student->phone ?></p>
+                <div class="portfolio-info">
+                  <h4><a href="#"><?= $student->name ?></a></h4>
+                  <p><?= $student->registration_number ?></p>
+                  <p><?= $student->ttl ?></p>
+                  <p><?= $student->address ?></p>
+                  <p><?= $student->phone ?></p>
+                </div>
               </div>
             </div>
-          </div>
+          <?php else: ?>
+            <p>Santri Tidak ditemukan</p>
+          <?php endif; ?>
+
 
         </div>
 
