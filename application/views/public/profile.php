7e029cb5df5dcea885b3d52743a0e21db801a7f9
madukubah <muhalfalah1998@gmail.com>
2020-10-26 16:29:10 +0800

    update

--- application/views/public/profile.php
+++ application/views/public/profile.php
@@ -90,11 +90,16 @@
             </p>
           </div>
         </div>
-        <div class="text-center" >
-          <iframe width="420" height="315"
-            src="https://www.youtube.com/embed/z8qjHeHa_uo">
-          </iframe> 
+        <div class="row">
+          <div class="col-lg-12 wow fadeInUp">
+            <div class="text-center" >
+              <iframe width="420" height="315"
+                src="https://www.youtube.com/embed/z8qjHeHa_uo">
+              </iframe> 
+            </div>
+          </div>
         </div>
+
         <div class="row">
           <div class="col-lg-12 wow fadeInUp">
             <p class="text-justify" >
